7f9a4521ca1f8f457ccc59a09f27e97d3e79c0a4
bebesf <89807577+bebesf@users.noreply.github.com>
2022-02-28 18:18:39 -0800

    this is a graded assignment

---
+++ e-commercewebsite/prod.js
@@ -0,0 +1,20 @@
+
+const courses = {
+    
+    java: 10,
+
+    javascript: 55,
+
+    nodejs: 5,
+
+    php: 15
+}; 
+
+courses.forEach(element => {
+    console.log(element.java)
+});
+
+
+// courses.forEach(function(key, index){
+//     console.log(key.java);
+// });
